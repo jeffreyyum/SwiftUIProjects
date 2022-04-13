c122b58e33a8eea90ce81117d53ffe9dc952491c
Jeffrey Yum <jeffreyyum@berkeley.edu>
2022-04-13 00:06:36 -0700

    hw 7 done

---
+++ hw7/newsfeedSkeleton/Article.swift
@@ -0,0 +1,35 @@
+//
+//  Article.swift
+//  newsfeedSkeleton
+//
+//  Created by Jeffrey Yum on 4/12/22.
+//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Welcome
+struct ArticleResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable, Identifiable {
+    
+    let id: UUID = UUID()
+    let author: String?
+    let url: String
+    let source, title, articleDescription: String
+    let image: String
+    let date: Date
+
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
---
+++ hw7/newsfeedSkeleton/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  newsfeedSkeleton
+//
+//  Created by Jeffrey Yum on 4/12/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings Screen")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
